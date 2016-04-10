530719b35d5fbdbe74fb4024bab31cde298d5ed0
Cream2015 <343224963@qq.com>
2016-04-10 10:32:49 +0800

    修复

--- voteM/src/vote/Controller/PageController.cs
+++ voteM/src/vote/Controller/PageController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,13 +16,19 @@ namespace vote.Controller
         [HttpGet]
         public IActionResult Show(int id)
         {
-            var photo = DB.Photos.Where(x => x.Id == id).SingleOrDefault();
+            var photo = DB.Photos.Include(x=>x.Author).Where(x => x.Id == id).SingleOrDefault();
             if (photo == null)
             {
                 return RedirectToAction("Error", "Home");
             }
             else
             {
+                //推荐作品
+                var Recommended = DB.Photos.Include(x => x.Author)
+                    .OrderByDescending(x => x.Priority)
+                    .ToList();
+                ViewBag.Recommended = Recommended;
+
                 return View(photo);
             }
             
